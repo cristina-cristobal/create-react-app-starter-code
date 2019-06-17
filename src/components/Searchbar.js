4c49176d003b9d5c66330d6209a8631cb0e1b1cd
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-06-17 12:37:19 -0400

    Revert "created cards"
    This reverts commit 9f0c4dffa5286fed72a6d4903c30479a48b8a16b.

--- src/components/Searchbar.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-const Searchbar = (props) => {
-  return (
-    <input className="search" placeholder="Search..." onChange={props.handleSearch}></input>
-  )
-}
-
-export default Searchbar
